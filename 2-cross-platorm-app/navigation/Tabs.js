a86dab4a8be0340b5b06f4770f1a200d20036a0d
vvlaads <vvlaads@gmail.com>
2026-04-04 21:40:24 +0300

    Added profile page

--- 2-cross-platorm-app/navigation/Tabs.js
+++ 2-cross-platorm-app/navigation/Tabs.js
@@ -1,6 +1,7 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Menu from '../components/Menu';
 import PortfolioScreen from '../screens/PortfolioScreen';
+import ProfileScreen from '../screens/ProfileScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -13,6 +14,7 @@ export default function Tabs() {
             tabBar={(props) => <Menu {...props} />}
         >
             <Tab.Screen name="Portfolio" options={{ title: 'Портфель' }} component={PortfolioScreen} />
+            <Tab.Screen name="Profile" options={{ title: 'Профиль' }} component={ProfileScreen} />
         </Tab.Navigator>
     );
 }
---
+++ 2-cross-platorm-app/screens/ProfileScreen.js
@@ -0,0 +1,100 @@
+import { View, Text, StyleSheet, Pressable } from 'react-native';
+import { fontSizes, fontWeights, typography } from '../theme/typography';
+import { colors } from '../theme/colors';
+
+export default function ProfileScreen({ navigation }) {
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Text style={[typography.title, { color: colors.white, marginTop: 50, marginBottom: 20 }]}>
+                    Иванов Алексей Владимирович
+                </Text>
+
+                <View style={styles.statisticsContainer}>
+                    <View style={styles.block}>
+                        <Text style={[typography.subtitle, { color: colors.main }]}>1.2М ₽</Text>
+                        <Text style={[typography.body, { color: colors.gray }]}>Портфель</Text>
+                    </View>
+
+                    <View style={styles.block}>
+                        <Text style={[typography.subtitle, { color: colors.green }]}>+4.2%</Text>
+                        <Text style={[typography.body, { color: colors.gray }]}>Доход</Text>
+                    </View>
+                </View>
+
+
+            </View>
+            <View style={styles.body}>
+                <Text style={typography.subtitle}>Операции</Text>
+                <View style={styles.optionContainer}>
+
+                </View>
+
+                <Text style={typography.subtitle}>Настройки</Text>
+                <View style={styles.optionContainer}>
+
+                </View>
+
+                <Pressable
+                    style={({ pressed }) => [
+                        styles.button,
+                        pressed ? styles.buttonHover : null
+                    ]}
+                    onPress={() => navigation.replace('Login')}
+                >
+                    <Text style={styles.buttonText}>Выйти из аккаунта</Text>
+                </Pressable>
+            </View>
+        </View >
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    header: {
+        backgroundColor: colors.main,
+        height: 350,
+        padding: 20,
+        borderBottomLeftRadius: 20,
+        borderBottomRightRadius: 20,
+    },
+    body: {
+        padding: 20,
+        justifyContent: 'top',
+        backgroundColor: colors.background,
+    },
+    block: {
+        backgroundColor: colors.white,
+        color: colors.black,
+        borderRadius: 20,
+        padding: 20,
+        width: '45%',
+        alignItems: 'center',
+        gap: 10
+    },
+    statisticsContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between'
+    },
+    optionContainer: {},
+    button: {
+        backgroundColor: colors.white,
+        paddingVertical: 12,
+        paddingHorizontal: 24,
+        borderRadius: 15,
+        borderWidth: 1,
+        borderColor: colors.red,
+        alignItems: 'center',
+    },
+    buttonHover: {
+        backgroundColor: colors.grayLight,
+    },
+    buttonText: {
+        color: colors.red,
+        fontWeight: fontWeights.default,
+        fontSize: fontSizes.medium,
+    },
+});
