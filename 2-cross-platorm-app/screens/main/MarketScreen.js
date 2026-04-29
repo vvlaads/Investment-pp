22f010980709b7283e784af0bbff7090372a4888
vvlaads <vvlaads@gmail.com>
2026-04-29 11:20:41 +0300

    Fixed: refactored assets on market page

--- 2-cross-platorm-app/screens/main/MarketScreen.js
+++ 2-cross-platorm-app/screens/main/MarketScreen.js
@@ -1,23 +1,44 @@
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
-import { typography } from '../../theme/typography';
+import { fontSizes, fontWeights, typography } from '../../theme/typography';
 import { palette } from '../../theme/palette';
 import AssetCard from '../../components/AssetCard';
 import arrow from '../../assets/icons/white/portfolio.png'
 import { useApp } from '../../utils/AppProvider';
 import { createCommonStyles } from '../../theme/commonStyles';
 import Search from '../../components/Search';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { FilterType } from '../../utils/FilterType';
 import Option from '../../components/Option';
+import { usePortfolio } from '../../hooks/usePortfolio';
+import LoadPage from '../../components/LoadPage';
+
 export default function MarketScreen({ navigation }) {
+    const { assets, loading } = usePortfolio();
     const { theme } = useApp();
     const common = createCommonStyles(theme);
     const [activeFilter, setActiveFilter] = useState(FilterType.ALL);
+    const [filteredAssets, setFilteredAssets] = useState([]);
+    const s = styles(theme);
 
     const selectFilter = (type) => {
         setActiveFilter(type);
         console.log('Выбрано', type);
     }
+
+    const handleSearch = (text) => {
+        const filtered = assets.filter(asset =>
+            asset.company.name.toLowerCase().includes(text.toLowerCase())
+        );
+
+        setFilteredAssets(filtered);
+    };
+
+
+    useEffect(() => {
+        setFilteredAssets(assets);
+    }, [assets]);
+
+    if (loading) return (<LoadPage />);
 
     return (
         <ScrollView style={common.container}
@@ -32,9 +53,7 @@ export default function MarketScreen({ navigation }) {
             <View style={common.body}>
                 <Search
                     style={common.search}
-                    onSearch={(text) => {
-                        console.log("Ищем", text);
-                    }}
+                    onSearch={(text) => handleSearch(text)}
                 />
 
                 <Text style={common.sectionName}>Выгодные предложения</Text>
@@ -45,22 +64,53 @@ export default function MarketScreen({ navigation }) {
                     <Option description={'Облигации'} value={FilterType.BONDS === activeFilter} onClick={() => selectFilter(FilterType.BONDS)} />
                 </View>
 
-                <View style={styles.assetsContainer}>
-                    <AssetCard companyName={'Apple'} amount={2} pricePerUnit={600} diffPerUnit={10} icon={arrow} navigation={navigation} />
-                    <AssetCard companyName={'Apple'} amount={2} pricePerUnit={600} diffPerUnit={10} icon={arrow} navigation={navigation} />
-                    <AssetCard companyName={'Apple'} amount={2} pricePerUnit={600} diffPerUnit={10} icon={arrow} navigation={navigation} />
-                    <AssetCard companyName={'Apple'} amount={2} pricePerUnit={600} diffPerUnit={10} icon={arrow} navigation={navigation} />
-                    <AssetCard companyName={'Apple'} amount={2} pricePerUnit={600} diffPerUnit={10} icon={arrow} navigation={navigation} />
-                    <AssetCard companyName={'Apple'} amount={2} pricePerUnit={600} diffPerUnit={10} icon={arrow} navigation={navigation} />
+                <View style={s.assetsContainer}>
+                    {filteredAssets.length > 0 ?
+                        filteredAssets.map((asset) => (
+                            <AssetCard
+                                key={asset.id}
+                                company={asset.company}
+                                amount={asset.amount}
+                                buyPrice={asset.buyPrice}
+                                icon={arrow}
+                                onPress={() => navigation.navigate('StockInfo')} />
+                        ))
+                        :
+                        (
+                            <View style={s.emptyBlock}>
+                                <Text style={s.emptyBlockText}>
+                                    Не найдено
+                                </Text>
+                            </View>
+                        )
+                    }
+
                 </View>
             </View>
         </ScrollView>
     );
 }
 
-const styles = StyleSheet.create({
+const styles = (theme) => StyleSheet.create({
     assetsContainer: {
         flexDirection: 'column',
         gap: 10,
+    },
+    emptyBlock: {
+        backgroundColor: theme.hover,
+        width: '100%',
+        height: 60,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 20,
+        borderColor: theme.border,
+        borderStyle: 'dashed',
+        borderWidth: 3,
+    },
+
+    emptyBlockText: {
+        color: theme.secondaryText,
+        fontSize: fontSizes.medium,
+        fontWeight: fontWeights.bold,
     },
 });
